7f3e7812ae976e6f2903ab0dd480379927581093
Uladzimir Vysotski <Uladzimir.Vysotski@easyjet.com>
2015-11-27 23:18:38 +0300

    InitialCommit and Task one completed

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter the natural number N");
+            int.TryParse(Console.ReadLine(), out n);
+            var cancellationTokenSource = new CancellationTokenSource();
+            var result = Task.Run(() => SumAsync(n, cancellationTokenSource.Token));
+            result.ContinueWith(task => Console.WriteLine("The result is: {0}", task.Result), TaskContinuationOptions.NotOnCanceled);
+
+            Console.WriteLine("Enter the natural number N");
+            while (int.TryParse(Console.ReadLine(), out n))
+            {
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource = new CancellationTokenSource();
+                result = Task.Run(() => SumAsync(n, cancellationTokenSource.Token));
+                result.ContinueWith(task => Console.WriteLine("The result is: {0}", task.Result), TaskContinuationOptions.NotOnCanceled);
+
+                Console.WriteLine("Enter the natural number N");
+            }
+
+            Console.WriteLine("The end");
+            Console.ReadLine();
+        }
+
+        private static int SumAsync(int n, CancellationToken ct)
+        {
+            var sum = 0;
+
+            for (var i = 0; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Thread.Sleep(3000);
+            ct.ThrowIfCancellationRequested();
+            return sum;
+        }
+    }
+}
