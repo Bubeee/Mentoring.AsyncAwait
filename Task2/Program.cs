b89b6c1f25ef57857d8e62311ba80fc3662a8c5d
Uladzimir Vysotski <Uladzimir.Vysotski@easyjet.com>
2015-11-28 11:59:27 +0300

    Task 2 completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Task2
 {
@@ -6,11 +10,41 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the url to download, e - to exit");
+            var url = Console.ReadLine();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            do
+            {
+                if (url.ToLower() == "c")
+                {
+                    cancellationTokenSource.Cancel();
+                    cancellationTokenSource = new CancellationTokenSource();
+                }
+                
+                Task.Run(() => DownloadAsync(url, cancellationTokenSource.Token), cancellationTokenSource.Token);
+
+                Console.WriteLine("Enter the url to download or c - to cancel all current downloads, e - to exit");
+                url = Console.ReadLine();
+            }
+            while (url.ToLower() != "e");
+
+            Console.WriteLine("The end");
+            Console.ReadLine();
         }
 
-        private static Task DownloadAsync()
+        private static async Task DownloadAsync(string url, CancellationToken ct)
         {
-            return null;
+            var uri = new Uri(url);
+            using (var client = new WebClient())
+            {
+                var result = await client.DownloadStringTaskAsync(uri);
+                Thread.Sleep(5000);
+                ct.ThrowIfCancellationRequested();
+                Console.WriteLine(result);
+                Console.WriteLine("Enter the url to download or c - to cancel all current downloads, e - to exit");
+            }
         }
     }
 }
